395a2fe2e9615f3a20942a0518ef7b40984767da
ryo watanabe <ryo.watanabe@theport.jp>
2024-06-20 17:03:48 +0900

    add:about page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   # http://127.0.0.1:3000/static_pages/help
   get 'static_pages/help'
+  # http://127.0.0.1:3000/static_pages/about
+  get 'static_pages/about'
   # http://
   root "application#hello"
 end
