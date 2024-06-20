5cc813a7f96993e157df2109820966c9f6f97051
ryo watanabe <ryo.watanabe@theport.jp>
2024-06-20 16:56:27 +0900

    add: controller spec

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe StaticPagesController, type: :controller do
+  describe "#home" do
+    it "responds successfully" do
+      get :home
+      expect(response).to be_successful
+    end
+  end
+
+  describe "#help" do
+    it "responds successfully" do
+      get :help
+      expect(response).to be_successful
+    end
+  end
+
+  describe "#about" do
+    it "responds successfully" do
+      get :about
+      expect(response).to be_successful
+    end
+  end
+end
