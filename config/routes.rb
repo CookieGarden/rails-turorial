56f1d63afaf3400b2c65d8dd6de41807c1c47f84
ryo watanabe <ryo.watanabe@theport.jp>
2024-06-24 18:03:11 +0900

    ルートコメントの簡略化

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@
 Rails.application.routes.draw do
   # http://127.0.0.1:3000/static_pages/home
   root  'static_pages#home'
+  # /
   get   'static_pages/home'
-  # http://127.0.0.1:3000/static_pages/help
+  # /help
   get   'static_pages/help'
-  # http://127.0.0.1:3000/static_pages/about
+  # /about
   get   'static_pages/about'
-  # http://127.0.0.1:3000/static_pages/about
+  # /contact
   get   'static_pages/contact'
 end
