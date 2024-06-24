71069c3d3281200e2bfff48e794be9fedc4aa27f
ryo watanabe <ryo.watanabe@theport.jp>
2024-06-24 17:16:09 +0900

    fix: specの場所を整理

---
+++ spec/system/static_pages_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'StaticPages', type: :system do
+  describe 'GET /' do
+    it 'has the correct title' do
+      visit root_path
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App')
+    end
+  end
+
+  describe 'GET /help' do
+    it 'has the correct title' do
+      visit static_pages_help_path
+      expect(page).to have_title('Help | Ruby on Rails Tutorial Sample App')
+    end
+  end
+
+  describe 'GET /about' do
+    it 'has the correct title' do
+      visit static_pages_about_path
+      expect(page).to have_title('About | Ruby on Rails Tutorial Sample App')
+    end
+  end
+end
