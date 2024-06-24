f752abc10c8398daf6b97d414fc9f66ef959d6a6
ryo watanabe <ryo.watanabe@theport.jp>
2024-06-24 18:15:33 +0900

    specに名前付きルーティングを適用

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,21 +12,21 @@ RSpec.describe 'StaticPages', type: :request do
 
   describe 'GET /help' do
     it 'responds successfully' do
-      get static_pages_help_path
+      get help_path
       expect(response).to have_http_status(200)
     end
   end
 
   describe 'GET /about' do
     it 'responds successfully' do
-      get static_pages_about_path
+      get about_path
       expect(response).to have_http_status(200)
     end
   end
 
   describe 'Get /contact' do
     it 'responds successfully' do
-      get static_pages_contact_path
+      get contact_path
       expect(response).to have_http_status(200)
     end
   end
--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -12,21 +12,21 @@ RSpec.describe 'StaticPages', type: :system do
 
   describe 'GET /help' do
     it 'has the correct title' do
-      visit static_pages_help_path
+      visit help_path
       expect(page).to have_title('Help | Ruby on Rails Tutorial Sample App')
     end
   end
 
   describe 'GET /about' do
     it 'has the correct title' do
-      visit static_pages_about_path
+      visit about_path
       expect(page).to have_title('About | Ruby on Rails Tutorial Sample App')
     end
   end
 
   describe 'GET /contact' do
     it 'has the correct title' do
-      visit static_pages_contact_path
+      visit contact_path
       expect(page).to have_title('Contact | Ruby on Rails Tutorial Sample App')
     end
   end
