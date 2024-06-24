6358f38c6bea837a92a8cd0aa95ebef7be7dedb6
ryo watanabe <ryo.watanabe@theport.jp>
2024-06-24 18:14:48 +0900

    ルートの再定義

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,12 @@
 
 Rails.application.routes.draw do
   # http://127.0.0.1:3000/static_pages/home
+  # root_path = /
+  # root_url  = http://127.0.0.1:3000/
   root  'static_pages#home'
-  # /
-  get   'static_pages/home'
-  # /help
-  get   'static_pages/help'
-  # /about
-  get   'static_pages/about'
-  # /contact
-  get   'static_pages/contact'
+  get   '/help',    to: 'static_pages#help'
+  get   '/about',   to: 'static_pages#about'
+  get   '/contact', to: 'static_pages#contact'
+  # contact_path = /contact
+  # contact_url  = root_url + contact_path
 end
